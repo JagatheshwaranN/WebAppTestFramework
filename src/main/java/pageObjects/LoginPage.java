d02ad6100150e574c9386e76775b2d3687882c92
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-02-11 19:36:36 +0530

    first commit

---
+++ src/main/java/pageObjects/LoginPage.java
@@ -0,0 +1,35 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    @FindBy(id = "loginForm")
+    WebElement loginForm;
+
+    @FindBy(xpath = "//h3[@class='form-title']")
+    WebElement loginFormTitle;
+
+    @FindBy(name = "login[email]")
+    WebElement email;
+
+    @FindBy(name="login[password]")
+    WebElement password;
+
+    @FindBy(id = "uniform-remember_me")
+    WebElement rememberMe;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    WebElement login;
+
+    @FindBy(xpath = "//a[contains(@href,'restorePassword')]")
+    WebElement forgotPassword;
+
+    public LoginPage(WebDriver driver){
+        PageFactory.initElements(driver, this);
+    }
+
+}
