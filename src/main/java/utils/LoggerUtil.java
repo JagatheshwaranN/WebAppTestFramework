b2f2b20626a0d295b5433584b21dd3da2d59046f
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-02-18 19:33:37 +0530

    LoggerUtil Implemented and Sample LogFile generated.

---
+++ src/main/java/utils/LoggerUtil.java
@@ -0,0 +1,103 @@
+package utils;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
+
+import java.io.File;
+import java.util.Objects;
+
+public class LoggerUtil {
+
+    private static final Logger logger = LogManager.getLogger();
+
+    public static synchronized void startTestCase(String testCaseName) {
+        testCaseName = testCaseName.replaceAll("[^a-zA-Z0-9]", "_").replaceAll("_+", "_");
+        startLog(System.getProperty("user.dir")+"/src/test/resources/logs/", testCaseName);
+        info("\n***************** Execution Started : " + testCaseName + "*****************");
+    }
+
+    public static void endTestCase(String testCaseName){
+        info("\n***************** Execution Ended : " + testCaseName + "*****************");
+    }
+
+    private static void startLog(String fileDirPath, String testCaseName) {
+        int numOfFiles = 0;
+        File directory = new File(fileDirPath);
+        if (directory.exists()) {
+            int count = 0;
+            for (File file : Objects.requireNonNull(directory.listFiles())) {
+                if (file.isFile() && file.getName().endsWith(".log") && file.getName().contains(testCaseName)) {
+                    count++;
+                }
+            }
+            numOfFiles = count;
+        }
+        numOfFiles++;
+        String logFileName = testCaseName + "_" + numOfFiles;
+        ThreadContext.put("logFileName", logFileName);
+    }
+
+    public static Logger getCurrentLog() {
+        return logger;
+    }
+
+    public static String getCallerInfo() {
+        String callerInfo;
+        String className = Thread.currentThread().getStackTrace()[3].getClassName();
+        String methodName = Thread.currentThread().getStackTrace()[3].getMethodName();
+        int lineNumber = Thread.currentThread().getStackTrace()[3].getLineNumber();
+        callerInfo = className + ":" + methodName + ":" + lineNumber + "->";
+        return callerInfo;
+    }
+
+    public static void trace(Object message) {
+        getCurrentLog().trace(message);
+    }
+
+    public static void trace(Object message, Throwable throwable) {
+        getCurrentLog().trace(message, throwable);
+    }
+
+    public static void debug(Object message) {
+        getCurrentLog().debug(getCallerInfo() + message);
+    }
+
+    public static void debug(Object message, Throwable throwable) {
+        getCurrentLog().debug(getCallerInfo() + message, throwable);
+    }
+
+    public static void error(Object message) {
+        getCurrentLog().error(getCallerInfo() + message);
+    }
+
+    public static void error(Object message, Throwable throwable) {
+        getCurrentLog().error(getCallerInfo() + message, throwable);
+    }
+
+    public static void fatal(Object message) {
+        getCurrentLog().fatal(getCallerInfo() + message);
+    }
+
+    public static void fatal(Object message, Throwable throwable) {
+        getCurrentLog().fatal(getCallerInfo() + message, throwable);
+    }
+
+    public static void info(Object message) {
+        getCurrentLog().info(getCallerInfo() + message);
+    }
+
+    public static void info(Object message, Throwable throwable) {
+        getCurrentLog().info(getCallerInfo() + message, throwable);
+    }
+
+    public static void warn(Object message) {
+        getCurrentLog().warn(getCallerInfo() + message);
+    }
+
+    public static void warn(Object message, Throwable throwable) {
+        getCurrentLog().warn(getCallerInfo() + message, throwable);
+    }
+
+}
--- src/test/java/testcases/TestCaseDemo.java
+++ src/test/java/testcases/TestCaseDemo.java
@@ -2,6 +2,7 @@ package testcases;
 
 import org.testng.annotations.Test;
 import testbase.TestBase;
+import utils.LoggerUtil;
 
 public class TestCaseDemo extends TestBase {
 
@@ -9,12 +10,16 @@ public class TestCaseDemo extends TestBase {
     public void testCase1() throws InterruptedException {
         Thread.sleep(10000);
         System.out.println("TestCase 1");
+        LoggerUtil.startTestCase(new Throwable().getStackTrace()[0].getMethodName());
+        LoggerUtil.info("TestCase 1 Execution");
     }
 
     @Test
     public void testCase2() throws InterruptedException {
         Thread.sleep(10000);
         System.out.println("TestCase 2");
+        LoggerUtil.startTestCase(new Throwable().getStackTrace()[0].getMethodName());
+        LoggerUtil.info("TestCase 2 Execution");
     }
 
 }
