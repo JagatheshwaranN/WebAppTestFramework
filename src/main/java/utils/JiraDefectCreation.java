d28ce75bf8f6e6d56cd3f04f1940c4c7e0006fb1
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-02-26 21:44:33 +0530

    Jira Defect Add Attachment Util Completed. DryRun Works Fine.

--- src/main/java/utils/JiraDefectCreation.java
+++ src/main/java/utils/JiraDefectCreation.java
@@ -5,6 +5,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Base64;
@@ -17,7 +18,8 @@ public class JiraDefectCreation {
     public static void main(String[] args) {
         String defectId = JiraDefectCreation.createDefectThruAPI("File Attachment Not Working", "File attachment option is not working and keep loading.",
                 "Bug", "AutomationDefect", "QDPM-1", "UAT", FileReader.getDataFromPropFile("jiraId"));
-        System.out.println(defectId);
+        System.out.println("Defect Created: " + defectId);
+        JiraDefectCreation.addAttachmentToDefect(defectId, new File(System.getProperty("user.dir")+"/reports/failure/24_02_2024_04_43_58.png"));
     }
 
     public static String createDefectThruAPI(String summary, String description, String issueType, String label, String parent, String environment, String assignee){
@@ -64,16 +66,36 @@ public class JiraDefectCreation {
                 .header("Authorization", "Basic "+basicAuthString)
                 .contentType(ContentType.JSON)
                 .body(defectPayload.toString())
-                .log()
-                .body()
         .when()
-                .post(FileReader.getDataFromPropFile("jiraDefectEndpoint"))
+                .post(FileReader.getDataFromPropFile("jiraDefectCreationEndpoint"))
         .then()
                 .statusCode(201)
                 .log()
                 .body()
                 .extract()
                 .path("id");
+    }
+
+
+    public static void addAttachmentToDefect(String defectId, File filePath){
+
+        byte[] basicAuth = Base64.getEncoder().encode((FileReader.getDataFromPropFile("jiraUserName")+":"+FileReader.getDataFromPropFile("jiraAccessToken")).getBytes());
+        String basicAuthString = new String(basicAuth);
+
+        String rawEndpoint = FileReader.getDataFromPropFile("jiraDefectAttachmentEndpoint");
+        String formattedEndpoint = rawEndpoint.replace("{issueIdOrKey}", defectId);
+
+        given()
+                .header("Authorization", "Basic "+basicAuthString)
+                .header("X-Atlassian-Token", "no-check")
+                .multiPart("file", filePath)
+        .when()
+                .post(formattedEndpoint)
+        .then()
+                .statusCode(200)
+                .log()
+                .body();
+        System.out.println("Attachment Added..");
     }
 
 }
